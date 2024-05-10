98975816b1d13df946e7a47fc0bc4262730e3796
Musarraf <muhammedmusarraf@gmail.com>
2024-05-10 14:39:15 +0530

    Learned Callback hell

--- index.js
+++ index.js
@@ -622,4 +622,100 @@
 
 // console.log("when hanlde error properly Programe reaches the end");
 
+//! Callback Hell
 
+//! the below code is synchronous
+// function task1(){
+//     console.log("Task 1 Complete");
+// }
+
+// function task2(){
+//     console.log("Task 2 Complete");
+// }
+
+// function task3(){
+//     console.log("Task 3 Complete");
+// }
+
+// function task4(){
+//     console.log("Task 4 Complete");
+// }
+
+// task1()
+// task2()
+// task3()
+// task4()
+// console.log("All Task Completed");
+
+//! the below code contains Asynchronous functions
+// function task1(){
+//     setTimeout(() => {
+//         console.log("Task 1 Complete");
+//     }, 2000);
+// }
+
+// function task2(){
+//     setTimeout(() => {
+//         console.log("Task 2 Complete");
+//     }, 1000);
+// }
+
+// function task3(){
+//     setTimeout(() => {
+//         console.log("Task 3 Complete");
+//     }, 3000);
+// }
+
+// function task4(){
+//     setTimeout(() => {
+//         console.log("Task 4 Complete");
+//     }, 1500);
+// }
+
+// task1()
+// task2()
+// task3()
+// task4()
+// console.log("All Task Completed");
+
+//! To make this work in order we can use callback OR PRomisses OR async/await
+//! Below is an example for the callback hell.
+
+// function task1(callback) {
+//   setTimeout(() => {
+//     console.log("Task 1 Complete");
+//     callback();
+//   }, 2000);
+// }
+
+// function task2(callback) {
+//   setTimeout(() => {
+//     console.log("Task 2 Complete");
+//     callback();
+//   }, 1000);
+// }
+
+// function task3(callback) {
+//   setTimeout(() => {
+//     console.log("Task 3 Complete");
+//     callback();
+//   }, 3000);
+// }
+
+// function task4(callback) {
+//   setTimeout(() => {
+//     console.log("Task 4 Complete");
+//     callback();
+//   }, 1500);
+// }
+
+// // callbackHell
+// task1(() => {
+//   task2(() => {
+//     task3(() => {
+//       task4(() => {
+//         console.log("All Task Completed");
+//       });
+//     });
+//   });
+// });
