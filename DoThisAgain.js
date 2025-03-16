d6e66c0665c488d33c3d922b05789ca6e462557f
Musarraf <muhammedmusarraf@gmail.com>
2025-03-16 08:59:52 +0530

    Added DoThisAgain.js

---
+++ DoThisAgain.js
@@ -0,0 +1,49 @@
+// const fruits = [
+//   { name: "apple", color: "red", calories: 80 },
+//   { name: "orange", color: "orange", calories: 60 },
+//   { name: "banana", color: "yellow", calories: 180 },
+//   { name: "pineapple", color: "yellow", calories: 37 },
+// ];
+
+//! Do this again
+// get max calorie fruit
+// get total calories
+// ----------------
+// sort by price
+// let items = [
+//     { name: "Apple", price: 30 },
+//     { name: "Banana", price: 20 },
+//     { name: "Orange", price: 25 },
+//   ];
+
+// ---------------------------------------
+//! Make the exucution in order 1,2,3,4 then all taks executed using callbackhell. and also in async await try catch method.
+// function task1(){
+//     setTimeout(() => {
+//         console.log("Task 1 Complete");
+//     }, 2000);
+// }
+
+// function task2(){
+//     setTimeout(() => {
+//         console.log("Task 2 Complete");
+//     }, 1000);
+// }
+
+// function task3(){
+//     setTimeout(() => {
+//         console.log("Task 3 Complete");
+//     }, 3000);
+// }
+
+// function task4(){
+//     setTimeout(() => {
+//         console.log("Task 4 Complete");
+//     }, 1500);
+// }
+
+// task1()
+// task2()
+// task3()
+// task4()
+// console.log("All Task Completed");
